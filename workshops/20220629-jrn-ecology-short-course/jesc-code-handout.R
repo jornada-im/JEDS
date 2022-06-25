0b6a551766abeb3a7b87a24d25b00cc4bb64f23f
Gregory E. Maurer <gregmaurer@gmail.com>
2022-06-25 13:55:04 -0600

    updates for multcomp and linear mm

--- workshops/20220629-jrn-ecology-short-course/jesc-code-handout.R
+++ workshops/20220629-jrn-ecology-short-course/jesc-code-handout.R
@@ -51,24 +51,40 @@ t.test(x = anpp.19.20.wide$`2019`,
        paired = TRUE)
 
 ##
-## Testing for unequal variance
+## How do we know which comparison (t-test) to use?
 ##
 
-# Create a 4-year data subset
-anpp.17.20 <- anpp %>%
-  dplyr::filter(year %in% 2017:2020)
+# Create a slightly different data subset - 2017 and 2018
+anpp.17.18 <- anpp %>%
+  dplyr::filter(year %in% 2017:2018)
 
-ggplot(anpp.17.20, aes(x = year, y = npp_g_m2, group = year)) +
+ggplot(anpp.17.18, aes(x = year, y = npp_g_m2, color = factor(year))) +
   geom_boxplot()
 
-library(car)
+# Bartlett test for unequal variance (homescedasticity)
+bartlett.test(npp_g_m2 ~ year, data = anpp.17.18)
 
-bartlett.test(npp_g_m2 ~ year, data = anpp.17.20)
-fligner.test(npp_g_m2 ~ year, data = anpp.17.20)
-leveneTest(npp_g_m2 ~ as.factor(year), data = anpp.17.20) # from car package
+# Test for normality of the data
+shapiro.test(anpp.17.18$npp_g_m2)
+
+# Fligner test for homoscedasticity
+fligner.test(npp_g_m2 ~ as.factor(year), data = anpp.17.18)
+
+# Levene test for homoscedasticity, which is in the `car` R package
+car::leveneTest(npp_g_m2 ~ as.factor(year), data = anpp.17.18)
+
+# Spread to wide form
+anpp.17.18.wide <- anpp.17.18 %>%
+  spread(year, npp_g_m2)
+
+# Paired t-test
+t.test(x = anpp.17.18.wide$`2017`, 
+       y = anpp.17.18.wide$`2018`, 
+       paired = TRUE,
+       equal_variance=FALSE) # This is the default, we don't have to specify
 
 ##
-## Comparing samples with general linear models
+## Comparing sample means with simple linear models
 ##
 
 # Two-sample mean comparison using a linear regression model
@@ -87,7 +103,8 @@ anpp.19.20.f$year <- factor(anpp.19.20.f$year)
 # One-way ANOVA equivalent to the 2-sample t-test
 aov.ttest <- aov(npp_g_m2 ~ year, data = anpp.19.20.f)
 summary(aov.ttest)
-car::Anova(lm.ttest, type = "III") # Not clear on difference in outputs, p value is same.
+# We could also use car::Anova here, but the results are the same
+#car::Anova(lm.ttest, type = "III")
 
 # Two-way ANOVA using year and vegetation zone
 aov.2way <- aov(npp_g_m2 ~ year + zone, data = anpp.19.20.f)
@@ -98,70 +115,81 @@ aov.1way.rm <- aov(npp_g_m2 ~ year + Error(site/year),
                    data = anpp.19.20.f)
 summary(aov.1way.rm)
 
-# ANOVA with 2017 data
-# Differences between vegetation zones
+##
+## Comparing group means with ANOVA results
+##
+
+# Subset to 2017
 anpp.2017 <- anpp %>%
   dplyr::filter(year == "2017") %>%
   mutate(zone = factor(zone))
 
-# Graph data with boxplots
+# Graph data by zone with boxplots
 ggplot(anpp.2017, aes(x = zone, y = npp_g_m2, fill = zone)) +
   geom_boxplot()
 
 # Run ANOVA with lm()
-lm.ANOVA <- lm(npp_g_m2 ~ zone, data = anpp.2017) 
-# Diagnostics
-plot(lm.ANOVA)
+lm.ANOVA <- lm(npp_g_m2 ~ zone, data = anpp.2017)
 
+summary(lm.ANOVA)
+
+car::Anova(lm.ANOVA, type = "III", test.statistic = "F")
+
+# Diagnostic plots - here we put them all on one page
+par(mfrow = c(2, 2), oma = c(0, 0, 2, 0)) -> opar
+plot(lm.ANOVA)
 
 # Test for normality of residuals
 resid(lm.ANOVA) %>% shapiro.test()
 
-# Look at model results.  Note that this reports Type I Sums of Squares
-summary(lm.ANOVA)
-# Note: recommend using Type III F tests from car::Anova()
-# In this case they are the same as Type I
-car::Anova(lm.ANOVA, type = "III", test.statistic = "F")
+##
+## Post-hoc tests and pairwise comparisons
+##
 
 library(emmeans)
 # Least squares means (estimated marginal means)
 emmeans(lm.ANOVA, ~ zone)
 
-# Notice there are n*(n-2)/2 possible pairwise comparisons
 # Pairwise comparisons of least squares means
 contrast(emmeans(lm.ANOVA, specs= ~zone), method = "pairwise")
 
 # Letter separations (efficient way to display)
-emmeans(lm.ANOVA, ~ zone) %>%
+emmeans.Tukey <- emmeans(lm.ANOVA, ~ zone) %>%
   multcomp::cld(Letters = LETTERS)
 
+emmeans.Tukey
+
 # Problem: emmeans switches to Sidak when adjust = "Tukey" is specified
+# I actually found this from Russ Lenth:
+#    https://stats.stackexchange.com/questions/508055/unclear-why-adjust-tukey-was-changed-to-sidak
+# It seems the confidence limits are being adjusted when you specify adjust = 
+# "Tukey" but not the means themselves. This is intended I guess, but I'm still
+# fuzzy on the statistical meaning.
 emmeans(lm.ANOVA, ~ zone, adjust = "Tukey") %>%
   multcomp::cld(Letters = LETTERS)
 
-# Compare results from Tukey and Scheffe
-emmeans.Tukey <- emmeans(lm.ANOVA, specs = ~ zone) %>% 
-  multcomp::cld(Letters = LETTERS) %>%
-  as_tibble() %>%
-  mutate(method = "Tukey")
-
+# First get comparisons from Scheffe
 emmeans.Scheffe <- emmeans(lm.ANOVA, specs = ~ zone) %>% 
   multcomp::cld(Letters = LETTERS, adjust = "scheffe") %>%
   as_tibble() %>%
   mutate(method = "Scheffe")
 
-# Look at least squares means and 95% confidence intervals
-bind_rows(emmeans.Tukey, emmeans.Scheffe) %>%
+# Put the Tukey results in a tibble
+emmeans.Tukey <- emmeans.Tukey %>% as_tibble() %>% mutate(method = "Tukey")
+
+# Look at least squares means and 95% confidence intervals for
+# Tukey and Scheffe
+bind_rows(emmeans.Scheffe, emmeans.Tukey) %>%
   mutate(.group = trimws(.group)) %>%
   ggplot(aes(x = zone, y = emmean, col = zone)) +
   geom_point() +
   geom_errorbar(aes(ymin=lower.CL, ymax=upper.CL), width=.1) +
-  geom_text(aes(x = zone, y = emmean, label = .group), hjust = - 0.5, show.legend = FALSE) +
+  geom_text(aes(x = zone, y = emmean, label = .group),
+            hjust = -0.5, show.legend = FALSE) +
   facet_wrap(~ method, ncol = 1) +
   ggtitle("2017 ANPP by Vegetation Zone", 
-          subtitle = "means with the same letter are not different at alpha = 0.05") +
+          subtitle = "Same-letter means are not different at alpha = 0.05") +
   ylab("Model-based mean ANPP (g/m^2) +/- 95% CI")
-
 
 # Look at least squares means and standard errors
 # This kind of figure is the one most commonly reported in scientific papers
@@ -170,10 +198,11 @@ bind_rows(emmeans.Tukey, emmeans.Scheffe) %>%
   ggplot(aes(x = zone, y = emmean, col = zone)) +
   geom_point() +
   geom_errorbar(aes(ymin=emmean-SE, ymax=emmean+SE), width=.1) +
-  geom_text(aes(x = zone, y = emmean, label = .group), hjust = - 0.5, show.legend = FALSE) +
+  geom_text(aes(x = zone, y = emmean, label = .group), 
+            hjust = - 0.5, show.legend = FALSE) +
   facet_wrap(~ method, ncol = 1) +
   ggtitle("2017 ANPP by Vegetation Zone", 
-          subtitle = "means with the same letter are not different at alpha = 0.05") +
+          subtitle = "Same-letter means are not different at alpha = 0.05") +
   ylab("Model-based mean ANPP (g/m^2) +/- S.E.")
 
 ##
@@ -183,79 +212,73 @@ bind_rows(emmeans.Tukey, emmeans.Scheffe) %>%
 library(nlme)
 library(lme4)
 
-# Repeated measures: 2017-2020
-# Compare Annual ANPP between 2019 and 2020
-anpp.2017.2020 <- anpp %>%
+# Subset the data between 2017 and 2020
+anpp.17.20 <- anpp %>%
   dplyr::filter(year %in% 2017:2020) 
 
 # Plot the data
-ggplot(anpp.2017.2020, aes(x = year, y = npp_g_m2, col = zone, group = site)) +
+ggplot(anpp.17.20, aes(x = year, y = npp_g_m2, col = zone, group = site)) +
   geom_point() +
   geom_line(linetype="dashed", size = .05) +
   scale_x_continuous(breaks = 2017:2020)
 
 # Variance is highest in 2017
-anpp.2017.2020 %>%
+anpp.17.20 %>%
   group_by(year) %>%
   summarise(std.dev = sd(npp_g_m2))
 
-# Fit a repeated measures model (correlated errors with unstructured structure)
+# Fit a repeated measures model (unstructured correlated errors)
 model.un <- gls(npp_g_m2 ~ zone*factor(year),
                 corSymm(form = ~ 1 | site),
                 weights=varIdent(form=~1|factor(year)),
-                data = anpp.2017.2020)
+                data = anpp.17.20)
 
-# Fit a repeated measures model (correlated errors with autoregressive covariance structure)
+# Fit a repeated measures model (correlated errors with autoregressive
+# covariance structure)
 model.ar <- gls(npp_g_m2 ~ zone*factor(year),  
                 corAR1(form = ~ 1 | site), 
                 weights=varIdent(form=~1|factor(year)), 
-                data = anpp.2017.2020)
+                data = anpp.17.20)
 
 AIC(model.un)
 AIC(model.ar)
 
-# AR actually fits better than Unstructured
-# Try using only 2 varaiance: parameters: one for 2017 when the varance in high 
-# Set 2018, 2019, 2020 variances to be estimated as the same 
-
-anpp.2017.2020 <- anpp.2017.2020 %>%
+# Assign 2017 to high variance group, other years to low
+anpp.17.20 <- anpp.17.20 %>%
   mutate(anpp_level = if_else(year == 2017, "high", "low")) 
 
-
+# Re-fit a model with a variance level
 model.ar <- gls(npp_g_m2 ~ zone*factor(year),  
                       corAR1(form = ~ 1 | site), 
                       weights=varIdent(form=~1|anpp_level), 
-                      data = anpp.2017.2020)
+                      data = anpp.17.20)
 
 AIC(model.ar)
 
 emmeans(model.ar, ~ zone*factor(year), lmer.df = "kenward-roger") 
 
-# It's a little difficult to extract the correlationa and variance parameters
+# Bet correlation and variance estimates
 coef(model.un$modelStruct$corStruct, uncons = FALSE, allCoef = TRUE)
 coef(model.un$modelStruct$varStruct, uncons = FALSE, allCoef = TRUE) 
-
-# Compare model results from 3 different mixed models specified similarly
-# Note differnt spceficiations in the funtion arguments
 
 # R-side model from nlme::gls()
 model.ar.level.gls <- gls(npp_g_m2 ~ zone*factor(year),  
                           corAR1(form = ~ 1 | site), 
                           weights=varIdent(form=~1|anpp_level), 
-                          data = anpp.2017.2020,
+                          data = anpp.17.20,
                           method = 'REML')
 
 # G-side model from nlme::lme()
 model.ar.level.lme <- lme(fixed = npp_g_m2 ~ zone*factor(year),  
                           random = ~ 1 | site,
                           weights = varIdent(form = ~ 1 | anpp_level),
-                          data = anpp.2017.2020,
+                          data = anpp.17.20,
                           method = 'REML')
 
 # G-side model from lme4::lmer()
 model.ar.level.lmer <- lmer(npp_g_m2 ~ zone*factor(year) + 
                               (anpp_level | site),
-                            data = anpp.2017.2020 %>%
+                            data = anpp.17.20 %>%
                               mutate(anpp_level = factor(anpp_level),
                                      site = factor(site)))
 
@@ -263,13 +286,16 @@ AIC(model.ar.level.gls)
 AIC(model.ar.level.lme)
 AIC(model.ar.level.lmer)
 
-mod.compare <- emmeans(model.ar.level.gls, ~zone*factor(year), lmer.df = "satterthwaite") %>%
+mod.compare <- emmeans(model.ar.level.gls, ~zone*factor(year),
+                       lmer.df = "satterthwaite") %>%
   multcomp::cld(alpha = 0.05, Letters = LETTERS) %>%
   mutate(model = "gls") %>%
-  bind_rows(emmeans(model.ar.level.lme, ~zone*factor(year), lmer.df = "satterthwaite") %>%
+  bind_rows(emmeans(model.ar.level.lme, ~zone*factor(year),
+                    lmer.df = "satterthwaite") %>%
               multcomp::cld(alpha = 0.05, Letters = LETTERS) %>%
               mutate(model = "lme")) %>%
-  bind_rows(emmeans(model.ar.level.lmer, ~zone*factor(year), lmer.df = "satterthwaite") %>%
+  bind_rows(emmeans(model.ar.level.lmer, ~zone*factor(year),
+                    lmer.df = "satterthwaite") %>%
               multcomp::cld(alpha = 0.05, Letters = LETTERS) %>%
               mutate(model = "lmer"))
 
